57eb5b4c2e12930dd50a10443d4ad3fb7f57c1d4
agui <2517405805@qq.com>
2025-12-11 20:58:08 +0800

    添加项目文件。

---
+++ Common/DifficultyLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+namespace EnhanceBoss.Common;
+public enum DifficultyLevel {
+    Normal,
+    Hard,
+    Expert,
+    Nightmare
+}
+public class DifficultyConfig : ModConfig {
+    public override ConfigScope Mode => ConfigScope.ServerSide;
+    [DefaultValue(DifficultyLevel.Normal)]
+    public DifficultyLevel GlobalDifficulty { get; set; } = DifficultyLevel.Normal;
+}
+internal static class DifficultyManager {
+    internal static DifficultyLevel CurrentDifficulty => ModContent.GetInstance<DifficultyConfig>().GlobalDifficulty;
+}
---
+++ Common/DifficultyUI.cs
@@ -0,0 +1,7 @@
+﻿using Terraria.ModLoader;
+namespace EnhanceBoss.Common;
+public class DifficultyUI : ModSystem {
+    public override void Load() {
+        base.Load();
+    }
+}
